b273297e4fbf249cd1ab413bcc4c7d9584acc57c
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-02 11:27:43 +0330

    Create Views for Pages APP

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
