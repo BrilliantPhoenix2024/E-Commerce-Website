f347e7a7eb62ccf1b4d28fb60e20302986e7d8f3
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-02 10:07:58 +0330

    CREATE accounts/forms.py & config it in settings.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+
 
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
