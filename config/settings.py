1bc5fe6f5c289663659c9d59fdc53f25bae0fb80
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-04 21:06:22 +0330

    Install Crispy-forms & Create _base.html

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'crispy-forms',
+
+    # local apps
     'accounts',
     'pages',
 ]
