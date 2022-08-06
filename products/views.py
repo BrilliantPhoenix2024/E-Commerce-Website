898aec9f3dede3b5aade29b85e7d97fa0f15cafe
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-06 14:03:04 +0330

    Register Product Model in 'products/admin.py' file

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from .models import Product
 
 
 class ProductListView(generic.ListView):
-    model = Product
+    # model = Product
+    queryset = Product.objects.filter(active=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
