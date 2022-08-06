a0000f09073acf70bba574312e713d6643786d16
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-06 10:59:08 +0330

    Create products/model.py & migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    # cover = models.ImageField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
