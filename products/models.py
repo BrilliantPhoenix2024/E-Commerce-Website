18c35dccbdea0fa830cde532350b21bca09b5b2f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-07 10:15:28 +0330

    Create get_absulote_url function for each product_detail & Modify product_list.html

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Product(models.Model):
@@ -13,5 +14,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])
 
 
