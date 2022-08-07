c22b5c1d417b120fd95f7d4ff6db201f91f9e108
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-07 10:07:57 +0330

    Create URL for product_detail in products/urls.py file

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
