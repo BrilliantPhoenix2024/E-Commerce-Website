0829851b75d155b766422614f0d870565520b4fd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-08 11:47:36 +0330

    Create COMMENT MODEL for comment section in products/models.py

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -17,5 +18,26 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Very good'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments',)
+    comment = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
 
 
