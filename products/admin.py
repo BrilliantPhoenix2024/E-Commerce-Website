058a727efb6c14355322967af2b9a41528531669
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-09 15:42:01 +0330

    Register Comment Model in products/admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active']
 
+
+@admin.register(Comment)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'comment', 'stars', 'active', ]
