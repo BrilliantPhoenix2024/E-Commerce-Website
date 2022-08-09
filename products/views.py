ee6ac9a0b7b42a3a3cc92e3f4e32eb739da66807
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-09 16:32:57 +0330

    Create CommentForm in products/forms.py & Edit views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django .views import generic
 
 from .models import Product
+from .forms import CommentForm
 
 
 class ProductListView(generic.ListView):
@@ -14,4 +15,9 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
 
