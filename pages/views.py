e92655429e964b1a2bb271dd18390908cc9b18cd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-11 18:43:47 +0330

    Create VIEW for Contact.html

--- pages/views.py
+++ pages/views.py
@@ -9,3 +9,8 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
