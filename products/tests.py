241fc4c6f4ac72ac556e064ebf58d93039871732
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-12 19:55:45 +0330

    ADD some test to [products/tests.py]

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .models import Product
+
+
+class ProductTest(TestCase):
+    def setUp(self):
+        self.product1 = Product.objects.create(
+            title='Product1',
+            description='this is a description of Product1',
+            active=True,
+            price=106,
+        )
+
+    def test_product_list_url(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_list_url_by_name(self):
+        response = self.client.get(reverse('product_list'))
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
