fc9d01633560ea66a6ec7ec8434fe955cadad0a0
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 18:00:51 +0330

    WRITE some another Tests for Pages APP in pages/tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -11,6 +11,10 @@ class PagesTest(TestCase):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
 
+    def test_about_page_template(self):
+        response = self.client.get('/about/')
+        self.assertTemplateUsed(response, 'pages/about.html')
+
     def test_home_page_url(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
@@ -19,11 +23,18 @@ class PagesTest(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
+    def test_home_page_template(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_contact_page_url(self):
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
-        # self.assertTemplateUsed(response, 'pages/contact.html')
 
     def test_contact_pages_url_by_name(self):
         response = self.client.get(reverse('contact'))
         self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_template(self):
+        response = self.client.get('/contact/')
+        self.assertTemplateUsed(response, 'pages/contact.html')
