4808c49b4e01699d07189d07b3c9575236df8f26
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 17:34:24 +0330

    WRITE Test for Pages APP in pages/tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class PagesTest(TestCase):
+    def test_about_page_url(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
