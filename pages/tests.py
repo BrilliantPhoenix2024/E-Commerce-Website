d887d48b099de40553a6a7a0dd187b40c03f6a7a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 17:54:14 +0330

    ADD more Tests to Pages APP

--- pages/tests.py
+++ pages/tests.py
@@ -18,3 +18,12 @@ class PagesTest(TestCase):
     def test_home_page_url_by_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_url(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'pages/contact.html')
+
+    def test_contact_pages_url_by_name(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
