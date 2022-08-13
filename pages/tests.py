9df2bb145b64ec7f1b0cf9dd00648703dd0f9bc1
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 17:37:48 +0330

    WRITE some Test for Pages APP in pages/tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -6,3 +6,7 @@ class PagesTest(TestCase):
     def test_about_page_url(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+
+    def test_about_page_url_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
