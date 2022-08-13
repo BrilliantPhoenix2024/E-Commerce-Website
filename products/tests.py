6a292f23d72b0198e9def5a03c08d79c2b3378fa
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 17:17:40 +0330

    ADD more Test to Products APP

--- products/tests.py
+++ products/tests.py
@@ -5,13 +5,22 @@ from .models import Product
 
 
 class ProductTest(TestCase):
-    def setUp(self):
-        self.product1 = Product.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        cls.product1 = Product.objects.create(
             title='Product1',
             description='this is a description of Product1',
             active=True,
             price=106,
         )
+        cls.product2 = Product.objects.create(
+            title='Product2',
+            description='this is a description of Product2',
+            active=False,
+            price=16,
+        )
+
+    # def setUp(self):
 
     def test_product_list_url(self):
         response = self.client.get('/products/')
@@ -21,8 +30,30 @@ class ProductTest(TestCase):
         response = self.client.get(reverse('product_list'))
         self.assertEqual(response.status_code, 200)
 
+    def test_product_title_on_product_list_page(self):
+        response = self.client.get(reverse('product_list'))
+        self.assertContains(response, self.product1.title)
 
+    def test_product_detail_url(self):
+        response = self.client.get(f'/products/{self.product1.id}/')
+        self.assertEqual(response.status_code, 200)
 
+    def test_product_detail_url_by_name(self):
+        response = self.client.get(reverse('product_detail', args=[self.product1.id]))
+        self.assertEqual(response.status_code, 200)
 
+    def test_product_description_on_product_detail_page(self):
+        response = self.client.get(reverse('product_detail', args=[self.product1.id]))  # f'/products/{self.product1.id}/'
+        self.assertContains(response, self.product1.title)
+        self.assertContains(response, self.product1.description)
+
+    def test_status_404_if_product_does_not_exist(self):
+        response = self.client.get(reverse('product_detail', args=[999]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_active_product_is_not_true(self):
+        response = self.client.get(reverse('product_list'))
+        self.assertContains(response, self.product1.title)
+        self.assertNotContains(response, self.product2.title)
 
 
