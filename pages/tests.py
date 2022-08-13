856f065753d0372c15972bb43ae295fd618ed2d5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-13 17:44:01 +0330

    WRITE more Tests for Pages APP in pages/tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -10,3 +10,11 @@ class PagesTest(TestCase):
     def test_about_page_url_by_name(self):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
