2326eeec5219d5a780985d42aae3265b58e335e6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-12 10:51:26 +0330

    Modify config/settings.py for context_processors

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom Context_processors
+                'cart.context_processors.cart',
             ],
         },
     },
