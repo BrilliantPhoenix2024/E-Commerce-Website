e205ff9b2d44409528b77a62bbdeab35ac003365
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-15 16:39:34 +0330

    Modify 'Product' Model for edit the 'datetime_created'

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -11,7 +12,8 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     cover = models.ImageField(verbose_name=_('Product Cover'), upload_to='product/product_cover/', blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
+    # datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_created = models.DateTimeField(default=timezone.now)
     datetime_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
