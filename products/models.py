28a19d73acf71a4e9252f2f2d0eff24ebdb967ac
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-21 11:13:55 +0330

    ADD 'short_description' to Product Model in products/models.py

--- products/models.py
+++ products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     # description = models.TextField()
     description = RichTextField()
+    short_description = models.CharField(max_length=300)
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     cover = models.ImageField(verbose_name=_('Product Cover'), upload_to='product/product_cover/', blank=True)
